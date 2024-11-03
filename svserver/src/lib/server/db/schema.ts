d1ede88a3ef577c4676e9d474b4497d4a7c58fa8
Alessandro Mezzogori <alessandro.mezzogori1802@gmail.com>
2024-11-03 15:24:58 +0100

    schema

--- svserver/src/lib/server/db/schema.ts
+++ svserver/src/lib/server/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, text, integer, timestamp } from 'drizzle-orm/pg-core';
+import { geometry, pgTable, serial, text, integer, timestamp, varchar, index } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
 	id: text('id').primaryKey(),
@@ -14,6 +14,21 @@ export const session = pgTable('session', {
 		.references(() => user.id),
 	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull()
 });
+
+export const device = pgTable('device', {
+	id: varchar('id', { length: 20 }).notNull(),
+	name: varchar('name', { length: 50 }).notNull(),
+	userid: text('userid').references(() => user.id).notNull(),
+})
+
+export const crashreport = pgTable('crashreport', {
+	id: serial('id').primaryKey(),
+	deviceid: varchar('deviceid', { length: 20 }).references(() => device.id).notNull(),
+	location: geometry('location', { type: 'point', mode: 'xy', srid: 4326 }).notNull(),
+	timestamp: timestamp('timestamp'),
+}, (t) => ({
+	spatialIndex: index('spatial_index').using('gist', t.location),
+}))
 
 export type Session = typeof session.$inferSelect;
 
