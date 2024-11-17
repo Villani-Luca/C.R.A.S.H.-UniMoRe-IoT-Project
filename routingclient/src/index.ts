78456dd38861c86bc6bfea6bb4c8e33a466ff182
Alessandro Mezzogori <alessandro.mezzogori1802@gmail.com>
2024-11-17 19:43:11 +0100

    rinominato wsserver

--- routingclient/src/index.ts
+++ routingclient/src/index.ts
@@ -11,16 +11,16 @@ console.info('Client is connected', client.connected);
 client.on('connect', () => {
   console.info("[MQQT] Connected");
 
-  client.subscribe('client-position-update', (err) => {
+  client.subscribe('client-position-update', (err: any) => {
     console.info('[MQTT] Client Position Update Subscription --- Error: ', err ?? 'none');
   })
 
-  client.subscribe('client-crash-notification', (err) => {
+  client.subscribe('client-crash-notification', (err: any) => {
     console.info('[MQTT] Client Crash Notification Subscription --- Error: ', err ?? 'none');
   })
 })
 
-client.on('error', (err) => {
+client.on('error', (err: any) => {
   console.error('[MQQT] Error', err);
 })
 
@@ -29,7 +29,7 @@ const topic_handlers: Record<string, ((msg: string) => Promise<void>) | undefine
   "client-crash-notification": (msg: string) => crash_client_notification(msg, client),
 };
 
-client.on('message', async (topic, data) => {
+client.on('message', async (topic: string, data: Buffer) => {
   const message = data.toString();
   console.debug(topic, message);
 
